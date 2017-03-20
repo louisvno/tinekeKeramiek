4c148b5d9f705006f43dc9710be5067ca320feac
louisvno <louisvn@gmail.com>
2017-03-20 21:44:01 +0100

    add error handeling

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -25,78 +25,58 @@ function getFiles () {
 * Returns an array of promises
 */
 function storeImages(cloudStorage, images, postId ){
-    var imgArr = [];
+    var imgs = [];
     
     for (var i=0;i < images.length; i++) {
         if(images[i].files.length > 0){  
           var imgName = images[i].files[0].name ;
           var file = images[i].files[0];
-          imgArr.push(getStorageRef(cloudStorage, postId, imgName).put(file));
+          imgs.push(getStorageRef(cloudStorage, postId, imgName).put(file));
         }
     }
-    return Promise.all(imgArr);
+    return Promise.all(imgs);
 }
-
-//first uppload all images, then push data to db
+/*
+*Function to send a new blog post and it's images
+*First uploads all images to cloud storage, then push data to db
+*/
 function sendPost(cloudStorage, database){ 
     var images = getFiles();
     var postRef = getPostRef(database);
-    
-    var storeImgs = storeImages(cloudStorage, images, postRef.postId)
-                    .then(function (result){
-                        var imgData ={};
-                        for (var i=0;i < result.length; i++) {
-                          if(images[i].files.length > 0){ 
-                            var data =   {
-                                        name: result[i].metadata.name,
-                                        publishDate: result[i].metadata.timeCreated,
-                                        storagePath: result[i].metadata.fullPath,
-                                        url: result[i].downloadURL
-                                       };
-                            imgData["img" + i] = data;
-                            }
-                        }
-                        return imgData;
-                    });
-
-    var storeData = function (imgData) {
-          if (!isEmptyObj(imgData)){ 
-            var d = new Date();
-            var postData = writePostData("user_id", d.toUTCString(), getFormInput(),imgData);
-            //post to db
-            //also a promise
-            postRef.path.set(postData);
-          } else {
-            throw new Error("data object empty");
-          }
-    }
-    
-    storeImgs.then(storeData);
+    //NOTE "then" also returns a promise, takes callback as parameter with one argument > fullfilment value
+    storeImages(cloudStorage, images, postRef.postId)
+        .catch(function (error){
+            console.log(error.message);
+        })
+        .then(function (result){
+            var imgData ={};
+            for (var i=0;i < result.length; i++) {
+              if(images[i].files.length > 0){ 
+                var data =   {
+                            name: result[i].metadata.name,
+                            publishDate: result[i].metadata.timeCreated,
+                            storagePath: result[i].metadata.fullPath,
+                            url: result[i].downloadURL
+                           };
+                imgData["img" + i] = data;
+                }
+            }
+           if (!isEmptyObj(imgData)){ 
+          //move date to write func
+                var d = new Date();
+                var postData = writePostData("user_id", d.toUTCString(), getFormInput(),imgData); 
+                //post to db returns Promise
+                return postRef.path.set(postData);
+           }else {
+                throw new Error("No files selected");
+           }
+           })
+        .then(function (imgData){
+            console.log("upload success!")
+        }).catch(function(error){
+            console.log(error.message);
+        });   
  }
-//    storeImgs.then(function (result){
-//         
-//        for (var i=0;i < result.length; i++) {
-//          if(images[i].files.length > 0){ 
-//            var data =   {
-//                        name: result[i].metadata.name,
-//                        publishDate: result[i].metadata.timeCreated,
-//                        storagePath: result[i].metadata.fullPath,
-//                        url: result[i].downloadURL
-//                       };
-//            imgData["img" + i] = data;
-//            }
-//        }
-//        
-//        if (!isEmptyObj(imgData)){ 
-//            var d = new Date();
-//            var postData = writePostData("user_id", d.toUTCString(), getFormInput(),imgData);
-//            //post to db
-//            //also a promise
-//            postRef.path.set(postData);
-//        }
-//    });
-//}
-
 
 function getFormInput() {
     return {
@@ -116,80 +96,10 @@ function writePostData(userId, date, formInput, imgData){
         images : imgData
     };
 }
-//returns a promise off all img download links
-//function getFileURLs(cloudStorage,images, postId){
-//    var imgURLs = [];
-//    
-//    for (var i=0;i < images.length; i++) {
-//      if(images[i].files.length > 0){ 
-//         var imgName = images[i].files[0].name;
-//         imgURLs.push(getStorageRef(cloudStorage, postId, imgName).getDownloadURL());
-//        }   
-//    }
-//    return Promise.all(imgURLs).then(function (URLs) {
-//       return URLs;
-//    });
-//}
 
-function writeImgData(cloudStorage,images, date, postId){
-   var imgObj = {};
-   var imgURLs = [];
-   
-//   for (var i=0;i < images.length; i++) {
-//      if(images[i].files.length > 0){ 
-//         var imgName = images[i].files[0].name;
-//         imgURLs.push(getStorageRef(cloudStorage, postId, imgName).getDownloadURL());
-//        }   
-//    }
-    
-//    Promise.all(imgURLs).then(function(url){ 
-//        for (var i=0;i < images.length; i++) {
-//          if(images[i].files.length > 0){ 
-//            var data =   {
-//                        name: images[i].files[0].name,
-//                        publishDate: date,
-//                        storagePath: postId + "/" + images[i].files[0].name,
-//                        url: url[i]
-//                       };
-//            imgObj["img" + i] = data;
-//            }
-//        }
-//        return imgObj;  
-//        
-//      }).catch(function(error){
-//        throw new Error("failed to get downloadurl");
-//      });
-}
 
 function isEmptyObj(obj){
     return Object.keys(obj).length < 1;
 }
 
 
-//var postData = writePostData(28,2992,"hello","world","music",[{link:"lll",name:"koll",desc:"kjkd"}])
-
-// var updates = {};
-//  updates['/posts/' + postId] = postData;
-
-
-
-// Try to create a user for ada, but only if the user id 'ada' isn't
-// already taken
-//var adaRef = firebase.database().ref('users/ada');
-//adaRef.transaction(function(currentData) {
-//  if (currentData === null) {
-//    return { name: { first: 'Ada', last: 'Lovelace' } };
-//  } else {
-//    console.log('User ada already exists.');
-//    return; // Abort the transaction.
-//  }
-//}, function(error, committed, snapshot) {
-//  if (error) {
-//    console.log('Transaction failed abnormally!', error);
-//  } else if (!committed) {
-//    console.log('We aborted the transaction (because ada already exists).');
-//  } else {
-//    console.log('User ada added!');
-//  }
-//  console.log("Ada's data: ", snapshot.val());
-//});
