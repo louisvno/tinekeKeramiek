5fba1340da58da36f803f7c86436e151930493ea
louisvno <louisvn@gmail.com>
2017-05-25 22:07:02 +0200

    add thumbnail generation

--- functions/index.js
+++ functions/index.js
@@ -19,6 +19,7 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
     const tempFilePath = "/tmp/${fileName}";
     const tempFilePathx500 = "/tmp/${fileName}_500";
     const tempFilePathx1000 = "/tmp/${fileName}_1000";
+    const tempFilePathThumb = "/tmp/${fileName}.png";
     // Exit if this is triggered on a file that is not an image.
     if (!contentType.startsWith('image/')) {
       console.log('This is not an image.');
@@ -32,8 +33,9 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
     //Generate custom uuid to be able to reference in database
     const uuid500 = UUID();
     const uuid1000 = UUID();
+    const uuidThumb = UUID();
     // Exit if the image is already a thumbnail.
-    if (fileName.startsWith('x500_') || fileName.startsWith('x1000_')) {
+    if (fileName.startsWith('x500_') || fileName.startsWith('x1000_')|| fileName.startsWith('thumb_')) {
       console.log('Already Processed.');
       return;
     }
@@ -48,15 +50,16 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
       .then(() => {
         console.log('Image downloaded locally to', tempFilePath);
         // Resize image using ImageMagick.
-        return Promise.all([spawn('convert',[ tempFilePath, '-resize', '500x500>', tempFilePathx500]),
-                            spawn('convert',[ tempFilePath, '-resize', '1000x1000>', tempFilePathx1000])
+        return Promise.all([spawn('convert',[ tempFilePath,'-strip', '-resize', '500x500>', tempFilePathx500]),
+                            spawn('convert',[ tempFilePath,'-strip', '-resize', '1000x1000>', tempFilePathx1000]),
+                            spawn('convert',[ tempFilePath,'-strip','-quality','80', '-thumbnail', '150x150>', 'PNG8:'+tempFilePathThumb])
                             ]);
         })
       .then(() => {
-        console.log('Image created at', tempFilePath);
         // Add a 'preview_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
         const newFilePathx500 = filePath.replace(/(\/)?([^\/]*)$/, `$1x500_$2`);
         const newFilePathx1000 = filePath.replace(/(\/)?([^\/]*)$/, `$1x1000_$2`);
+        const newFilePathThumb = filePath.replace(/(\/)?([^\/]*)$/, `$1thumb_$2`) +".png";
         // Uploading the thumbnail.
         //docs https://googlecloudplatform.github.io/google-cloud-node/#/docs/storage/1.1.0/storage/bucket?method=upload
         return Promise.all([bucket.upload(tempFilePathx500, {  
@@ -72,7 +75,14 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
                                                     metadata:{firebaseStorageDownloadTokens: uuid1000},
                                                     contentType: 'image/jpeg'
                                                     }                                    
-                                            })                                        
+                                            }),
+                            bucket.upload(tempFilePathThumb, {  
+                                              destination: newFilePathThumb,
+                                              metadata: {
+                                                    metadata:{firebaseStorageDownloadTokens: uuidThumb},
+                                                    contentType: 'image/png'
+                                                    }                                    
+                                            }) 
                             ]);
       }).then((imgs)=>{
       /*** 
@@ -80,24 +90,31 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
       *@docs https://googlecloudplatform.github.io/google-cloud-node/#/docs/storage/1.1.0/storage/file?method=getMetadata
       *file = data[0];
       ***/
-        return Promise.all([imgs[0][0].getMetadata(), imgs[1][0].getMetadata()]);
+        return Promise.all([imgs[0][0].getMetadata(), imgs[1][0].getMetadata(), imgs[2][0].getMetadata()]);
 
       }).then((metaData) => {
           //write thumb metadata to firebase database
           //generate the download url with uuid
           return Promise.all([admin.database()
-                                 .ref('x500Imgs/' + postId)
+                                 .ref(functions.config().database.x500path + "/" +postId)
                                  .push({
                                         timeCreated: metaData[0][0].timeCreated,
                                         path: metaData[0][0].name,
                                         downloadUrl: "https://firebasestorage.googleapis.com/v0/b/" + metaData[0][0].bucket + "/o/" +        encodeURIComponent(metaData[0][0].name) + "?alt=media&token=" + uuid500 
                                         }),
                               admin.database()
-                                 .ref('x1000Imgs/' + postId)
+                                 .ref(functions.config().database.x1000path + "/"+postId)
                                  .push({
                                         timeCreated: metaData[1][0].timeCreated,
                                         path: metaData[1][0].name,
                                         downloadUrl: "https://firebasestorage.googleapis.com/v0/b/" + metaData[1][0].bucket + "/o/" +        encodeURIComponent(metaData[1][0].name) + "?alt=media&token=" + uuid1000 
+                                        }),
+                               admin.database()
+                                 .ref(functions.config().database.thumbpath + "/"+postId)
+                                 .push({
+                                        timeCreated: metaData[2][0].timeCreated,
+                                        path: metaData[2][0].name,
+                                        downloadUrl: "https://firebasestorage.googleapis.com/v0/b/" + metaData[2][0].bucket + "/o/" +        encodeURIComponent(metaData[2][0].name) + "?alt=media&token=" + uuidThumb
                                         })
                               ]);
       })
